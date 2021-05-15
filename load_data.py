8ce7d05b7c8005b31c6a2b9efdcd57e354e43137
pontusolausson <pontusolausson@hotmail.com>
2021-05-15 13:09:31 +0200

    Changed dataloader, dataset, and added data images

--- load_data.py
+++ load_data.py
@@ -2,32 +2,40 @@ import os
 from torch.utils.data import DataLoader
 from segmentation_dataset import SegmentationDataset
 import matplotlib.pyplot as plt
+import numpy as np
+import torch
 
 from torchvision import datasets, transforms
 
-train_path = "/Users/pontusolausson/supervised/Agriculture-Vision-2021/train/small/images/nir"
-train_labels_path = "/Users/pontusolausson/supervised/Agriculture-Vision-2021/train/small/labels/"
+train_path = "small_dataset/images/nir/"
+train_labels_path = "small_dataset/labels/"
 
 train_img_index = os.listdir(train_path)
 
 labels = {}
 labels_one_hot = {}
+k = 0
 for i, (root, dirs, filenames) in enumerate(os.walk(train_labels_path)):
+    if i == 0:
+        k = len(dirs)
+
     if (root != train_labels_path):
         label = root.replace(train_labels_path, "")
         labels[label] = filenames
 
-        labels_one_hot[label] = [0 for j in range(9)]
+        labels_one_hot[label] = np.zeros((k,))
         labels_one_hot[label][i - 1] = 1
 
-dataset = SegmentationDataset(train_img_index, labels)
+dataset = SegmentationDataset(train_img_index, labels, labels_one_hot)
 
 batch_size = 1
 shuffle = True
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
-image, label = next(iter(dataloader))
-#for image_batch, label_batch in iter(dataloader):
-#    print(image_batch)
+#image, label = next(iter(dataloader))
+#print(label)
+
+for image_batch, label_batch in iter(dataloader):
+    print(torch.max(label_batch))
 
 print("finished")
--- segmentation_dataset.py
+++ segmentation_dataset.py
@@ -1,23 +1,38 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 from skimage.io import imread
+import numpy as np
 
 class SegmentationDataset(Dataset):
-    def __init__(self, inputs, targets, transform=None):
+    def __init__(self, inputs, targets, one_hot, transform=None):
         self.inputs = inputs
-        self.targets = targets['waterway']
+        self.targets = targets
+        self.one_hot = one_hot
         self.transform = transform
-        # self.inputs_dtype = torch.float32
-        # self.targets_dtype = torch.long
 
     def __len__(self):
         return len(self.inputs)
 
     def __getitem__(self, index):
-        input_ID = "/Users/pontusolausson/supervised/Agriculture-Vision-2021/train/small/images/nir/" + self.inputs[index]
-        target_ID = "/Users/pontusolausson/supervised/Agriculture-Vision-2021/train/small/labels/waterway/" + self.targets[index]
+        input_ID = "small_dataset/images/nir/" + self.inputs[index]
+        x = imread(input_ID)
 
-        x, y = imread(input_ID), imread(target_ID)
+        label_path = "small_dataset/labels/"
+        y = None
+        for label in self.targets:
+            yy = imread(label_path + label + "/" + self.targets[label][index])
+
+            # Extract the label pixel value (+1 since 0 is the value for background pixels)
+            label_num = int(np.where(self.one_hot[label]==1)[0]) + 1
+
+            # Replace the white pixels (val=255) with the label pixel value
+            yy[yy==255] = label_num
+
+            # This is built on the assumption that no 2 labels can cover the same area of an image
+            if y is None:
+                y = yy
+            else:
+                y += yy
 
         if self.transform is not None:
             x, y = self.transform(x, y)
