eb27d751f458adfff727d1dd7700e86091b809ac
Ali Shibli <alishibli97@hotmail.com>
2021-06-08 21:52:13 +0200

    delete unecessary

--- train.py
+++ train.py
@@ -1,123 +1,211 @@
+# dataloader code
 from unet import *
-from segmentation_dataset import *
-from load_data import *
-
-#dataloader code
-
-import os
-from torch.utils.data import DataLoader
 from segmentation_dataset import SegmentationDataset
+from torch.nn import DataParallel
+
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import os
+from loguru import logger
 
+from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+from tqdm import tqdm, trange
+import random
 
 def listdir_nohidden(path):
     for f in os.listdir(path):
         if not f.startswith('.'):
             yield f
 
-train_path = "small_dataset/images/nir/"
-val_path = "small_dataset/images/nir/"
-train_labels_path = "small_dataset/labels/"
+notali = False
+if notali:
+        ali = "../../../alishibli6/DD2424-Deep-Learning-Project/"
+else:
+        ali = ""
+train_path =ali + "../Data/Agriculture-Vision-2021/train/images/nir/"
+val_path = ali+"../Data/Agriculture-Vision-2021/val/images/nir/"
+test_path =ali+ "../Data/Agriculture-Vision-2021/test/images/nir/"
+train_labels_path =ali+ "../Data/Agriculture-Vision-2021/train/labels/"
+val_labels_path =ali+ "../Data/Agriculture-Vision-2021/val/labels/"
+test_labels_path =ali+ "../Data/Agriculture-Vision-2021/test/labels/"
 
-train_img_names_index = os.listdir(train_path)
-val_img_names_index = os.listdir(val_path)
+# train_path = "../Data/Agriculture-Vision-2021/train/images/nir/"
+# val_path = "../Data/Agriculture-Vision-2021/val/images/nir/"
+# test_path = "../Data/Agriculture-Vision-2021/test/images/nir/"
+
+# train_labels_path = "../Data/Agriculture-Vision-2021/train/labels/"
+# val_labels_path = "../Data/Agriculture-Vision-2021/val/labels/"
+# test_labels_path = "../Data/Agriculture-Vision-2021/test/labels/"
+
+#train_path = "small_dataset/images/nir/"
+#val_path = "small_dataset/images/nir/"
+#test_path = "small_dataset/images/nir/"
+
+#train_labels_path = "small_dataset/labels/"
+#val_labels_path = "small_dataset/labels/"
+#test_labels_path = "small_dataset/labels/"
+
+train = os.listdir(train_path)
+val = os.listdir(val_path)
+test = os.listdir(test_path)
+
+random.shuffle(train)
+random.shuffle(val)
+random.shuffle(test)
+
+train_img_names_index = train[:10000]
+val_img_names_index = val[:2000]
+test_img_names_index = test[:2000]
 
 labels_one_hot = {}
-k = 9
-for i, label in enumerate(listdir_nohidden(train_labels_path)):
-    labels_one_hot[label] = np.zeros((k,))
-    labels_one_hot[label][i] = 1
+k = 8
+i=0
+for label in listdir_nohidden(train_labels_path):
+    if label!="storm_damage":
+        labels_one_hot[label] = np.zeros((k,))
+        labels_one_hot[label][i] = 1
+        i+=1
 
-train_dataset = SegmentationDataset(train_img_names_index, labels_one_hot)
-val_dataset = SegmentationDataset(val_img_names_index, labels_one_hot)
- 
-batch_size = 1
-shuffle = True
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+train_dataset = SegmentationDataset("train", train_img_names_index, labels_one_hot, train_path, train_labels_path, use_cache=True)
+val_dataset = SegmentationDataset("validation", val_img_names_index, labels_one_hot, val_path, val_labels_path, use_cache=True)
+#test_dataset = SegmentationDataset("test", test_img_names_index, labels_one_hot, test_path, test_labels_path, use_cache=True)
 
-
-print("finished")
-
-#SETTINGS
+# SETTINGS
 Use_GPU = True
-Lr = 0.01
-channels = 1 # 512*512 #pixels
-classes = 10 # outputs = number of classes + 1
-maxEpochs = 100
-batch_size = 1
+Lr = 1e-2
+channels = 1  # NIR vs RGB
+classes = 9  # outputs (9 labels + 1 background)
+maxEpochs = 30
+batch_size = 64
 shuffle = True
 
-#Code 
+# Code 
 if Use_GPU: 
     if torch.cuda.is_available():
         device = torch.device('cuda')
+        logger.info('cuda used')
     else:
         device = torch.device('cpu')
 else:
     device = torch.device('cpu')
-#initalize model 
+# initalize model 
 
-#fix activationfunc, dropout and other settings for model as parameters later 
+# fix activationfunc, dropout and other settings for model as parameters later 
 
-model = UNet(channels,classes).to(device)
+model = UNet(channels, classes).to(device)
+model = DataParallel(model, device_ids=range(10),output_device=range(10))
 
-#two different filepaths
-pathTrain = "Agriculture-Vision-2021\train"
-pathVal = "Agriculture-Vision-2021\val"
+trainValRate = 0.7  # not in use
+lrRatesplan = None  # not in use
+activation = "relu"  # not in use 
 
-trainValRate = 0.7 #not in use
-lrRatesplan = None #not in use
-activation = "relu" #not in use 
-criterion = torch.nn.CrossEntropyLoss()
+class_weights = torch.FloatTensor([1]+[5]*8).cuda()
+criterion = nn.CrossEntropyLoss(weight=class_weights)
+
 optimizer = torch.optim.SGD(model.parameters(), Lr)
-
 
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
 val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=shuffle)
+# test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
 
-# trainY = SegmentationDataset(train_img_names_index, labels_one_hot)
-# validateY = SegmentationDataset(train_img_names_index, labels_one_hot)
-
-#trainer class/object?
-
-from tqdm import tqdm, trange
-
+trainingAcc = []
 trainingLoss = []
+validationAcc = []
 validationLoss = []
 
 def itterProgress(x, text = "training"):
     return tqdm(enumerate(x), text, total = len(x))
 
 def run(): 
-    itter = itterProgress(trainX)
-    for i in range(maxEpochs):
-        pass      
+    # itter = itterProgress(trainX)
 
-def train(): 
-    
-    # model.train() #does this even work? 
+    for epoch in range(maxEpochs):
+        train(epoch)
 
+        if epoch%3==0:
+            torch.save(model.state_dict(), f"trained_model_{epoch}.pth")
+
+            f = open(f"history_{epoch}.txt","w")
+            for i in range(len(trainingAcc)):
+                str = f"acc_train={trainingAcc[i]},acc_loss={trainingLoss[i]},val_acc={validationAcc[i]},val_loss={validationLoss[i]}\n"
+                f.write(str)
+
+
+def train(epoch):
+    model.train()
     for i, (batch_x, batch_y) in enumerate(train_dataloader):
         indata, target = batch_x.to(device), batch_y.to(device)
         optimizer.zero_grad()
+        indata = indata.unsqueeze(1)
+        out = model(indata)
+        out_softmax = torch.softmax(out, 1)
+        img = postprocess(out_softmax)
         
-#         print(indata.shape)
-        
-        indata = indata.unsqueeze(0)
-
-        out = model.forward(indata)
-        # out = model(indata[None, ...])
+        train_acc = iou(img, target)
         loss = criterion(out, target)
-        loss_value = loss.item()
-
-        trainingLoss.append(loss_value)
+        train_loss = loss.item()
+        
+        trainingAcc.append(train_acc)
+        trainingLoss.append(train_loss)
 
         loss.backward()
         optimizer.step()
 
+        val_acc, val_loss = validate()
+
+        validationAcc.append(val_acc)
+        validationLoss.append(val_loss)
+
+        logger.info(f"Epoch {epoch} batch {i+1}/{len(train_dataloader)} loss={train_loss} acc={train_acc} val_loss={val_loss} val_acc={val_acc}")
+
+        if val_loss > np.mean(validationLoss)*1.5:
+            logger.info("Overfitting detected")
+            break
+
+def validate():
+    model.eval()
+    validationAcc_temp = []
+    validationLoss_temp = []
+    for i, (batch_x, batch_y) in enumerate(val_dataloader):
+        indata, target = batch_x.to(device), batch_y.to(device)
+        
+        with torch.no_grad():
+            indata = indata.unsqueeze(1)
+            out = model.forward(indata)
+            out_softmax = torch.softmax(out, 1)
+            img = postprocess(out_softmax)
+            
+            val_acc = iou(img, target)            
+            loss = criterion(out, target)
+            val_loss = loss.item()
+
+            validationAcc_temp.append(val_acc)
+            validationLoss_temp.append(val_loss)
     
-train()
-print(trainingLoss)
+    return np.mean(validationAcc_temp),np.mean(validationLoss_temp)
+
+def postprocess(img):
+    img = torch.argmax(img, dim=1)
+    img = img.cpu().numpy()
+    img = np.squeeze(img)
+    img = torch.from_numpy(img).type(torch.int64)
+    img = img.to(device)
+    # img = re_normalize(img)
+    return img
+
+def iou(prediction, target):
+    eps = 0
+    score = 0
+
+    for k in range(1, 10):
+        intersection = torch.sum((prediction==target) * (target==k)).item()
+        union = torch.sum(prediction==k).item() + torch.sum(target==k).item()
+        iou_k = 0 if intersection == 0 else (intersection + eps) / (union + eps)
+        score += iou_k
+
+    score = score / 9
+    return score
+
+run()
